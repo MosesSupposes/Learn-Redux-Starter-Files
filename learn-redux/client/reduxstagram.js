75bd5839bc7bb3427de5d964aa7bca5ab71738e5
Moses Samuel <mosamuel98@gmail.com>
2019-04-06 14:52:28 -0700

    app layout + component setup

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+// Dependencies 
+import React from 'react'
+import { render } from 'react-dom'
+
+// Css
+import css from './styles/style.styl'
+
+// Components
+import Main from './components/Main'
+
+render(<Main/>, document.getElementById('root'))
