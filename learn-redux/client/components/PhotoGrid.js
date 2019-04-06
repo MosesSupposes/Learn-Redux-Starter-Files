2e6632ff034ab49be86cb20c3421bd3569d2c36c
Moses Samuel <mosamuel98@gmail.com>
2019-04-06 15:05:53 -0700

    create Single and PhotoGrid components

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'React'
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+                I'm the photo grid
+            </div>
+        )
+    }
+})
+
+export default PhotoGrid
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'React'
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+                I'm the single 
+            </div>
+        )
+    }
+})
+
+export default Single
